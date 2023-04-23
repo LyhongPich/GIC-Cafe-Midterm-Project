5cf004df8c289c5f8e4125f57e977dce4fbebdef
Pich Lyhong <lyhong.pich@gmail.com>
2023-04-23 20:56:27 +0700

    Spring Boot Project

---
+++ Cafe-Management/src/main/java/gic/i4b/group6/CafeManagement/ServletInitializer.java
@@ -0,0 +1,13 @@
+package gic.i4b.group6.CafeManagement;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CafeManagementApplication.class);
+	}
+
+}
